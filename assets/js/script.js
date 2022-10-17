646c2b1fbf13b8c9c9bc1a47927e0bd4a7ca04d8
MohamedJamh <jam7.mohamed@gamil.com>
2022-10-17 11:35:47 +0100

    last version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
-var auto_id = 1;
+loadData();
+var auto_id;
 
 
-var title = document.getElementById('txt_title');
+var txt_title = document.getElementById('txt_title');
 var rbtn_bug = document.getElementById('rbtn_bug');
 var select_status = document.getElementById('select_status');
 var select_priority = document.getElementById('select_priority');
 var date_picker = document.getElementById('date_picker');
 var area_description = document.getElementById('area_description');
 
-function printData(){
+function addtask(task){
+
+}
+function loadData(){
     for (t of tasks){
         let table , task_icon;
 
@@ -26,31 +30,66 @@ function printData(){
         // creating task button
         const task = document.createElement("button");
         task.classList.add("task", "d-flex", "p-3", "w-100", "border-0", "border-bottom");
-        let id = t.id;
-        let title = t.title;
-        let date = t.date;
-        let description = t.description.slice(0,55);
-        let description_argument = t.description;
-        let priority = t.priority;
-        let type = t.type;
         task.innerHTML = `
         <span class="task-status">
             <i class="${task_icon}"></i>
         </span>
         <div class="task-body text-start pl-3">
-            <div class="task-title">${title}</div>
+            <div class="task-title">${t.title}</div>
             <div class="task-details">
-                <div class="task-date">#${id} created in ${date}</div>
-                <div class="task-description" title="${description_argument}">${description}...</div>
+                <div class="task-date">#${t.id} created in ${t.date}</div>
+                <div class="task-description" title="${t.description}">${t.description.slice(0,55)}...</div>
             </div>
             <div class="task-features">
-                <span class="task-priority p-2 rounded-3">${priority}</span>
-                <span class="task-type p-2 rounded-3">${type}</span>
+                <span class="task-priority p-2 rounded-3">${t.priority}</span>
+                <span class="task-type p-2 rounded-3">${t.type}</span>
             </div>
         </div>`
-        task.setAttribute("id",auto_id)
+        task.setAttribute("id",t.id)
         table.append(task);
-        auto_id++;
+        auto_id = t.id + 1;
     }
+}
+function ajouter(){
     
+    let table , task_icon;
+    if( select_status.value == 'to-do'){
+        table = document.querySelector(".to-do .table-body");
+        task_icon = 'bi bi-question-circle-fill text-green';
+    }else if ( select_status.value == 'in-progress'){
+        table = document.querySelector(".in-progress .table-body");
+        task_icon = 'spinner-border text-green';
+    }else{
+        table = document.querySelector(".done .table-body");
+        task_icon = 'bi bi-check-circle-fill text-green';
+    }
+
+    // creating task button
+    const task = document.createElement("button");
+    task.classList.add("task", "d-flex", "p-3", "w-100", "border-0", "border-bottom");
+
+    let type ;
+    if(rbtn_bug.checked){
+        type = 'Bug';
+    }else{
+        type = 'Feature';
+    }
+    task.innerHTML = `
+    <span class="task-status">
+        <i class="${task_icon}"></i>
+    </span>
+    <div class="task-body text-start pl-3">
+        <div class="task-title">${txt_title.value}</div>
+        <div class="task-details">
+            <div class="task-date">#${auto_id} created in ${date_picker.value}</div>
+            <div class="task-description" title="${area_description.value}">${area_description.value.slice(0,55)}...</div>
+        </div>
+        <div class="task-features">
+            <span class="task-priority p-2 rounded-3">${select_priority.value}</span>
+            <span class="task-type p-2 rounded-3">${type}</span>
+        </div>
+    </div>`
+    task.setAttribute("id",auto_id);
+    table.append(task);
+    auto_id++;
 }
